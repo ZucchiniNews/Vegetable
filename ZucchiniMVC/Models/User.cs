943628fdcbd260732431610ae8313194e850db29
Penny Teng <penny.hp.teng@gmail.com>
2026-03-23 15:26:44 +0100

    add subscriptions in user class

--- ZucchiniMVC/Models/User.cs
+++ ZucchiniMVC/Models/User.cs
@@ -4,14 +4,13 @@ namespace Zucchinimvc.Models
 {
     public class User : IdentityUser
     {
-        // Basic profile info
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime? DateOfBirth { get; set; }
 
         public bool IsActive { get; set; } = true;
 
-        //    public ICollection<Subscription> Subscriptions { get; set; } = new List<Subscription>();
+        public ICollection<Subscription> Subscriptions { get; set; } = new List<Subscription>();
 
         //    public ICollection<UserCategoryPreference> Preferences { get; set; }
 
