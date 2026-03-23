a4666ac8cde7861a514c05d48437cedbc869831b
Penny Teng <penny.hp.teng@gmail.com>
2026-03-23 16:07:30 +0100

    update some comments

--- ZucchiniMVC/Models/SubscriptionType.cs
+++ ZucchiniMVC/Models/SubscriptionType.cs
@@ -3,15 +3,10 @@
     public class SubscriptionType
     {
         public int Id { get; set; }
-
         public string TypeName { get; set; } // e.g., "Monthly", "Yearly"
-
         public string Description { get; set; } = string.Empty;
-
         public decimal Price { get; set; }
-
         public int DurationInDays { get; set; }
-
         public ICollection<Subscription> Subscriptions { get; set; }
     }
 }
