a4563b4abadb006618bff0b43c9a8abf633a06b6
Penny Teng <penny.hp.teng@gmail.com>
2026-03-24 14:35:02 +0100

    remove the default string value from WeatherVM

--- ZucchiniMVC/Models/ViewModels/WeatherViewModel.cs
+++ ZucchiniMVC/Models/ViewModels/WeatherViewModel.cs
@@ -2,10 +2,10 @@
 {
     public class WeatherViewModel
     {
-        public string City { get; set; } = "Stockholm";
+        public string City { get; set; } = string.Empty;
         public double Temp { get; set; }
         public string Description { get; set; } = string.Empty;
-        public string Icon { get; set; } = "";
+        public string Icon { get; set; } = string.Empty;
         public string IconUrl => $"https://openweathermap.org/img/wn/{Icon}@2x.png"; 
     }
 }
