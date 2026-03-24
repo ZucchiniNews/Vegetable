5b1563998c05d734659c3734ab49c2294919fb29
Penny Teng <penny.hp.teng@gmail.com>
2026-03-24 11:31:32 +0100

    create weatherVM

---
+++ ZucchiniMVC/Models/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Zucchinimvc.Models.ViewModels
+{
+    public class WeatherViewModel
+    {
+        public string City { get; set; } = "Linköping";
+        public double Temp { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+    }
+}
