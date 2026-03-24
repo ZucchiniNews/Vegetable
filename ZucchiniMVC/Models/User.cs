be5eff1029691cefa2a43681a4e05533394adccf
Penny Teng <penny.hp.teng@gmail.com>
2026-03-24 08:44:54 +0100

    set string.Empty to FN and LN in User class

--- ZucchiniMVC/Models/User.cs
+++ ZucchiniMVC/Models/User.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Zucchinimvc.Models
 {
     public class User : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public DateTime? DateOfBirth { get; set; }
 
         public bool IsActive { get; set; } = true;
