b257748182da2bf391d811daae73410b53c64a3a
Penny Teng <penny.hp.teng@gmail.com>
2026-03-26 14:24:57 +0100

    add cityWeatherChart class

---
+++ ZucchiniMVC/Models/ViewModels/CityWeatherChart.cs
@@ -0,0 +1,10 @@
+﻿namespace Zucchinimvc.Models.ViewModels
+{
+    public class CityWeatherChart
+    {
+        public string City { get; set; } = "City";
+        public List<DateTime> Labels { get; set; } = new List<DateTime>();
+        public List<double> Temperatures { get; set; } = new List<double>();
+    }
+
+}
