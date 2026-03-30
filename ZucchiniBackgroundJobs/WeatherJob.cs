5dd38bdab2a8bd7a14b99569840d70834feab28f
Penny Teng <penny.hp.teng@gmail.com>
2026-03-30 16:31:50 +0200

    change RunOnStartup back to false

--- ZucchiniBackgroundJobs/WeatherJob.cs
+++ ZucchiniBackgroundJobs/WeatherJob.cs
@@ -16,7 +16,7 @@ public class WeatherJob
     }
 
     [Function("WeatherJob")]
-    public async Task Run([TimerTrigger("0 0 0,6,12,18 * * *", RunOnStartup = true)] TimerInfo myTimer)
+    public async Task Run([TimerTrigger("0 0 0,6,12,18 * * *", RunOnStartup = false)] TimerInfo myTimer)
 
     {
         var cities = new[] { "Linköping", "Stockholm", "Oslo", "Helsinki", "Copenhagen" };
