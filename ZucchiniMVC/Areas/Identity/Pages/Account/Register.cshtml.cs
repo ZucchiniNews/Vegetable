60fdc56a1fe13aa01e3e8567693ea869d6da82a5
Penny Teng <penny.hp.teng@gmail.com>
2026-03-31 15:15:36 +0200

    add FName, LName, DoB in Register.cshtml.cs

--- ZucchiniMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ZucchiniMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -75,6 +75,21 @@ namespace Zucchinimvc.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
+
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [DataType(DataType.Date)]
+            [Display(Name = "Date of Birth")]
+            public DateTime DateOfBirth { get; set; }
+
+
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
