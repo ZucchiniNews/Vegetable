d7188c529aa47eb56e7532dbe6ffb3381f7b35db
Penny Teng <penny.hp.teng@gmail.com>
2026-04-01 14:56:59 +0200

    create a DTO folder for weather. (Original WeatherReponse.cs made into 3 .cs)

---
+++ ZucchiniMVC/Models/WeatherDTOs/WeatherInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace Zucchinimvc.Models.WeatherDTOs;
+
+public class WeatherInfo
+{
+    public string Description { get; set; } = string.Empty;
+    public string Icon { get; set; } = string.Empty;   // <img src="https://openweathermap.org/img/wn/@Model.Weather[0].Icon@2x.png" alt="Weather icon" />
+}
---
+++ ZucchiniMVC/Models/WeatherDTOs/WeatherResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Zucchinimvc.Models.WeatherDTOs;
+public class WeatherResponse
+{
+    public MainInfo? Main { get; set; }
+    public List<WeatherInfo>? Weather { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
+
+
+
+
