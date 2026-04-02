df094379b3936ae0e808e069b436a212e7bcf2ad
odinous95 <mjd.thif@icloud.com>
2026-04-02 09:31:50 +0100

    Add CmsController with async Index action for articles
    Introduced CmsController in Zucchinimvc.Controllers, injecting ICmsService. Added Index action to asynchronously fetch and display articles using the service.

---
+++ ZucchiniMVC/Controllers/CmsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zucchinimvc.Controllers
+{
+    public class CmsController : Controller
+    {
+        private readonly ICmsService _cmsService;
+
+        public CmsController(ICmsService cmsService)
+        {
+            _cmsService = cmsService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var articles = await _cmsService.GetArticles();
+            return View(articles);
+        }
+    }
+}
