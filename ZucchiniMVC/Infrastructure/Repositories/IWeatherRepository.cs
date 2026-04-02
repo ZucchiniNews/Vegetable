75a9f0c8a77a49f6886d5b1536473a6f49345c41
Penny Teng <penny.hp.teng@gmail.com>
2026-04-02 13:32:02 +0200

    create IWeatherRepository

---
+++ ZucchiniMVC/Infrastructure/Repositories/IWeatherRepository.cs
@@ -0,0 +1,10 @@
+﻿using ZucchiniCore.Entities;
+using Zucchinimvc.Models.DTOs.WeatherDTOs;
+
+namespace Zucchinimvc.Infrastructure.Repositories;
+
+public interface IWeatherRepository
+{
+    Task<GeoLocation?> GetCoordinatesAsync(string city);
+    Task<WeatherResponse?> GetWeatherAsync(double lat, double lon);
+}
