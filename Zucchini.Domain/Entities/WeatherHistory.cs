4e9eca277d71f022c58e447e23617aee377e7123
Penny Teng <penny.hp.teng@gmail.com>
2026-04-10 11:09:42 +0200

    add a comment in WeatherHistory

--- Zucchini.Domain/Entities/WeatherHistory.cs
+++ Zucchini.Domain/Entities/WeatherHistory.cs
@@ -1,4 +1,5 @@
 ﻿namespace Domain.Entities;
+// Represents a saved record for the predefined cities — lives in Azure Table
 public class WeatherHistory
 {
     public string City { get; set; } = "Linköping";
