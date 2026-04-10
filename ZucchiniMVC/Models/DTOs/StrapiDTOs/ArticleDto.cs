bd290253ac67255db44ff1c8a7609a927acbec50
odinous95 <mjd.thif@icloud.com>
2026-04-10 09:46:43 +0100

    Remove DocumentId property from ArticleDto
    The DocumentId string property was removed from the ArticleDto class, so ArticleDto no longer includes this field. This change may affect any code that relied on accessing or setting DocumentId in ArticleDto instances.

--- ZucchiniMVC/Models/DTOs/StrapiDTOs/ArticleDto.cs
+++ ZucchiniMVC/Models/DTOs/StrapiDTOs/ArticleDto.cs
@@ -1,7 +1,6 @@
 public class ArticleDto
 {
     public int Id { get; set; }
-    public string DocumentId { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Slug { get; set; } = string.Empty;
