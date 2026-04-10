ecc11dfdc492eb4f34402a4243c4aaf7080cb3f6
Penny Teng <penny.hp.teng@gmail.com>
2026-04-10 13:22:18 +0200

    delete apiLogger

--- Zucchini.Application/Services/Logger/IApiLoggerService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Application.Services.Logger;
-
-public interface IApiLoggerService
-{
-    void LogApiWarning(string apiName, string message);
-    void LogApiError(string apiName, Exception ex);
-}
