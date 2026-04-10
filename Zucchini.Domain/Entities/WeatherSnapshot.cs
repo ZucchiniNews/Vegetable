e81b6434178763cbdf62698a6edf12d80a4e575a
Penny Teng <penny.hp.teng@gmail.com>
2026-04-10 11:07:51 +0200

    add weatherSnapshot for lookup current weather for any city.

---
+++ Zucchini.Domain/Entities/WeatherSnapshot.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities;
+
+// Represents a live API fetch result
+public class WeatherSnapshot
+{
+    public string City { get; set; } = string.Empty;
+    public double Temperature { get; set; }
+    public int Humidity { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Icon { get; set; } = string.Empty;
+}
