466e15ae712d89af092e0122fdba5d769f118466
Myasar Kamala <mesokamala@ymail.com>
2026-04-13 15:00:22 +0200

    DTO and Strapi Keyupdated

---
+++ ZucchiniMVC/Models/DTOs/CurrencyDTOs/CurrencyDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Zucchinimvc.Models.DTOs.CurrencyDTOs
+{
+    public class CurrencyDTO
+    {
+        public Dictionary<string, decimal> Rates { get; set; }
+        public string Base { get; set; }
+        public string Date { get; set; }
+        public bool Success { get; set; }
+    }
+}
