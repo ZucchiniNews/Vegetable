810d48fe08813d4ded4d9bdc163f6b91a7ce6ef8
Myasar Kamala <mesokamala@ymail.com>
2026-04-13 15:59:24 +0200

    clientapi pt 1

---
+++ ZucchiniMVC/Infrastructure/ApiClients/CurrencyClient/ICurrencyApiClient.cs
@@ -0,0 +1,9 @@
+﻿using Zucchinimvc.Models.DTOs.CurrencyDTOs;
+
+namespace Zucchinimvc.Infrastructure.ApiClients.CurrencyClient
+{
+    public interface ICurrencyApiClient
+    {
+        Task<CurrencyRateDTO>
+    }
+}
