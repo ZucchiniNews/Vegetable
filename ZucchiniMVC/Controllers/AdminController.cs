b6702548b22974e0754cc0def6361523f7c1649f
Penny Teng <penny.hp.teng@gmail.com>
2026-04-16 13:05:52 +0200

    add admin and user logic in User Management Index razor view page

---
+++ ZucchiniMVC/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ZucchiniCore.Entities;
+
+namespace Zucchinimvc.Controllers;
+
+public class AdminController : Controller
+{
+    public async Task SetupAdminAsync(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+
+        string roleName = "Admin";
+        if (!await roleManager.RoleExistsAsync(roleName))
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+
+        var user = await userManager.FindByEmailAsync("ZucchiniNews@gmail.com");
+
+        if (user != null)
+        {
+            if (!await userManager.IsInRoleAsync(user, roleName))
+            {
+                await userManager.AddToRoleAsync(user, roleName);
+            }
+        }
+    }
+
+    [Authorize(Roles = "Admin")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
