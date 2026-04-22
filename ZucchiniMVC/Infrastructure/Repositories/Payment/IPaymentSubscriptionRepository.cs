95e06e8526d0185b6cf8d635d0f95a4d7dff62a1
Odin Thif <mjd.thif@icloud.com>
2026-04-22 15:34:36 +0200

    feat: add GetSubscriptionByIdAsync method to IPaymentSubscriptionRepository interface

--- ZucchiniMVC/Infrastructure/Repositories/Payment/IPaymentSubscriptionRepository.cs
+++ ZucchiniMVC/Infrastructure/Repositories/Payment/IPaymentSubscriptionRepository.cs
@@ -1,9 +1,11 @@
 
 
+using ZucchiniCore.Entities;
+
 namespace ZucchiniMVC.Infrastructure.Repositories.Payment
 {
     public interface IPaymentSubscriptionRepository
     {
-
+        Task<Subscription> GetSubscriptionByIdAsync(int subscriptionId);
     }
 }
