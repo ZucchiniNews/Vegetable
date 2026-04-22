4e609c78001e339dd778b67b1caba4f7b08b2275
Odin Thif <mjd.thif@icloud.com>
2026-04-22 15:34:25 +0200

    feat: implement PaymentSubscriptionRepository to manage subscription data retrieval

--- ZucchiniMVC/Infrastructure/Repositories/Payment/PaymentSubscriptionRepository.cs
+++ ZucchiniMVC/Infrastructure/Repositories/Payment/PaymentSubscriptionRepository.cs
@@ -1,7 +1,21 @@
+using ZucchiniCore.Entities;
+using Zucchinimvc.Infrastructure.Data;
+
 namespace ZucchiniMVC.Infrastructure.Repositories.Payment
 {
     public class PaymentSubscriptionRepository : IPaymentSubscriptionRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public PaymentSubscriptionRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Subscription> GetSubscriptionByIdAsync(int subscriptionId)
+        {
+            return await _context.Subscriptions.FindAsync(subscriptionId);
+        }
 
     }
 }
