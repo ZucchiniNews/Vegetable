114bce563d53d3a252448eefe6d23dff5587d044
odinous95 <mjd.thif@icloud.com>
2026-04-28 16:58:18 +0100

    Remove unused using directives from Plan service/repo files
    Clean up redundant imports (System.Collections.Generic, System.Threading.Tasks) in IPlanService.cs, PlanService.cs, IPlanRepository.cs, and PlanRepository.cs to improve code clarity and maintainability.

--- ZucchiniMVC/Application/Services/Plans/IPlanService.cs
+++ ZucchiniMVC/Application/Services/Plans/IPlanService.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using ZucchiniCore.Entities;
 
 namespace Zucchinimvc.Application.Services.Plans
--- ZucchiniMVC/Infrastructure/Repositories/PlanRepo/PlanRepository.cs
+++ ZucchiniMVC/Infrastructure/Repositories/PlanRepo/PlanRepository.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ZucchiniCore.Entities;
 using Zucchinimvc.Infrastructure.Data;
