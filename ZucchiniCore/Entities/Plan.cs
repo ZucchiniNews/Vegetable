a661546d5f985307d8b51557e7cf867f56ce8bab
odinous95 <mjd.thif@icloud.com>
2026-04-29 10:11:36 +0100

    Rename ProviderPriceId to StripePriceId in Plan class
    Renamed the ProviderPriceId property to StripePriceId to better reflect its purpose. No other changes were made to the Plan class.

--- ZucchiniCore/Entities/Plan.cs
+++ ZucchiniCore/Entities/Plan.cs
@@ -6,6 +6,6 @@
         public required string Name { get; set; }
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public required string ProviderPriceId { get; set; }
+        public required string StripePriceId { get; set; }
     }
 }
