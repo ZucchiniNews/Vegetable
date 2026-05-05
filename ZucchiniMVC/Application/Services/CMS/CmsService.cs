5e710c90279408dd92e15b3202037758b76fac82
Penny Teng <penny.hp.teng@gmail.com>
2026-05-05 10:51:35 +0200

    add like logic in CmsService article Details

--- ZucchiniMVC/Application/Services/CMS/CmsService.cs
+++ ZucchiniMVC/Application/Services/CMS/CmsService.cs
@@ -1,14 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using ZucchiniCore.Entities;
+using Zucchinimvc.Application.Services.Articles;
+using Zucchinimvc.Application.Services.Subscriptions;
 using Zucchinimvc.Infrastructure.Repositories.CmsRepo;
+using Zucchinimvc.Models.ViewModels;
+
 
 namespace Zucchinimvc.Application.Services.CMS;
 
 public class CmsService : ICmsService
 {
     private readonly ICmsRepository _cmsRepository;
-    public CmsService(ICmsRepository cmsRepository)
+    private readonly IUtilsService _utilsService;
+    private readonly ISubscriptionService _subscriptionService;
+    public CmsService(ICmsRepository cmsRepository, IUtilsService utilsService, ISubscriptionService subscriptionService)
     {
         _cmsRepository = cmsRepository;
+        _utilsService = utilsService;
+        _subscriptionService = subscriptionService;
     }
 
     public async Task<IEnumerable<Article>> GetArticles()
@@ -29,6 +39,36 @@ public class CmsService : ICmsService
         return article;
     }
 
+
+    public async Task<IActionResult> Details(string slug)
+    {
+        // 1. Get the article from CmsService
+        var article = await GetArticleBySlug(slug);
+
+        // 2. Get the current user ID
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        // 3. Get Like data from UtilsService
+        var likeCount = await _utilsService.GetLikeCountAsync(article.Id);
+        var isLiked = await _utilsService.IsLikedByUserAsync(article.Id, userId);
+        bool hasActiveSubscription = false;
+        if (!string.IsNullOrEmpty(userId))
+        {
+            hasActiveSubscription = await _subscriptionService.UserHasActiveSubscription(userId);
+        }
+
+        article.ReadingTimeMinutes = _utilsService.CalculateReadTime($"{article.BodyPreview} {article.BodyGated}");
+
+        var viewModel = new ArticleViewModel
+        {
+            Article = article,
+            LikeCount = likeCount,
+            IsLikedByCurrentUser = isLiked,
+            IsSubscribed = hasActiveSubscription
+        };
+
+        return View(viewModel);
+    }
     public async Task<List<Category>> GetCategories()
     {
         var categories = await _cmsRepository.GetCategoriesAsync();
