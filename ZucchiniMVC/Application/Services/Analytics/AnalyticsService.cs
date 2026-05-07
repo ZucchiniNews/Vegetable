a168453889362275d7cc9038e2187cc6b2ac84d7
Karl Danielsson <karl.d98@hotmail.com>
2026-05-07 10:52:40 +0200

    Updating stripe functionality with trackers

--- ZucchiniMVC/Application/Services/Analytics/AnalyticsService.cs
+++ ZucchiniMVC/Application/Services/Analytics/AnalyticsService.cs
@@ -10,12 +10,12 @@ public class AnalyticsService : IAnalyticsService
 {
     private readonly IAzureInsightClient _InsightClient;
     private readonly LogsQueryClient _logsQueryClient;
-    private readonly string _workspaceId;
+    private readonly string _resourceId;
     public AnalyticsService(IAzureInsightClient InsightClient, LogsQueryClient logsQueryClient, IConfiguration configuration)
     {
         _InsightClient = InsightClient;
         _logsQueryClient = logsQueryClient;
-        _workspaceId = configuration["ApplicationInsights:WorkspaceId"]!;
+        _resourceId = configuration["ApplicationInsights:ResourceId"]!;
     }
 
     public async Task TrackAsync(EventType eventType, string resourceId, string? userId = null)
@@ -39,34 +39,42 @@ public class AnalyticsService : IAnalyticsService
             | summarize TotalRequests = count(), UniqueUsers = dcount(user_Id)
             // customEvents
             // | where timestamp between(datetime({from:O}) .. datetime({to:o}))";
-    
-        var topArticlesQuery = $@"
+
+        var response = await _logsQueryClient.QueryResourceAsync(
+            new ResourceIdentifier(_resourceId),
+            query,
+            QueryTimeRange.All
+        );
+
+         var topArticlesQuery = $@"
             customEvents
-            | whre timestamp >= ago(30d)
+            | where timestamp >= ago(30d)
             | where name == 'ArticleView'
-            | summarize ViewsCount = count() by ResourceId = tostring(customDimensions['ResourceId'])
+            | summarize ViewCount = count() by ResourceId = tostring(customDimensions['ResourceId'])
             | order by ViewCount desc
             | take 5";
 
         var topArticlesResponse = await _logsQueryClient.QueryResourceAsync(
-            new ResourceIdentifier("/subscriptions/f6c79cfc-dcff-4cec-8cc0-8cbbd35495fa/resourceGroups/Gr25-17RG/providers/microsoft.insights/components/ZucchiniNews"),
+            new ResourceIdentifier(_resourceId),
             topArticlesQuery,
-            QueryTimeRange.All
-        );
-        
-        var response = await _logsQueryClient.QueryResourceAsync(
-            new ResourceIdentifier("/subscriptions/f6c79cfc-dcff-4cec-8cc0-8cbbd35495fa/resourceGroups/Gr25-17RG/providers/microsoft.insights/components/ZucchiniNews"),
-            query,
             QueryTimeRange.All
         );
 
         var table = response.Value.Table;
         var row = table.Rows.FirstOrDefault();
+        var topArticles = topArticlesResponse.Value.Table.Rows
+            .Select(row => new TopArticleDto
+        {
+            ResourceId = (string)row["ResourceId"],
+            ViewCount = (int)(long)row["ViewCount"]
+        })
+        .ToList();
         
         return new AnalyticsSummaryDto
         {
             Views = row != null ? (int)(long)row["TotalRequests"] : 0,
             UniqueVisitors = row != null ? (int)(long)row["UniqueUsers"] : 0,
+            TopArticles = topArticles
         };
     }
 }
