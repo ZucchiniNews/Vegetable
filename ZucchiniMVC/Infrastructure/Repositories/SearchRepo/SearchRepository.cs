aa2eaea8d85b5e279ce1076fe658935180e368f8
odinous95 <mjd.thif@icloud.com>
2026-05-07 16:34:43 +0100

    Refactor search repo to use ZucchininSearchClient
    Updated ISearchRepository to return IEnumerable<ArticlesSearchResultDTO> instead of string. Refactored SearchRepository to use ZucchininSearchClient and updated SearchGetResultAsync to process and return search results as DTOs. Removed unused code and dependencies on the old SearchClient.

--- ZucchiniMVC/Infrastructure/Repositories/SearchRepo/SearchRepository.cs
+++ ZucchiniMVC/Infrastructure/Repositories/SearchRepo/SearchRepository.cs
@@ -1,22 +1,28 @@
-﻿using Microsoft.Extensions.Options;
-using System.Net.Http.Headers;
-using Zucchinimvc.Infrastructure.ApiClients.SearchClient;
-using Zucchinimvc.Infrastructure.Config;
+﻿
+using Zucchinimvc.Infrastructure.ApiClients.ZucchininSearchClient;
+using Zucchinimvc.Models.DTOs.SearchDTOs;
 
 namespace Zucchinimvc.Infrastructure.Repositories.SearchRepo
 {
     public class SearchRepository : ISearchRepository
     {
-        private readonly SearchClient _searchClient;  
+        private readonly ZucchininSearchClient _zucchininSearchClient;
 
-        public SearchRepository(SearchClient searchClient)
+        public SearchRepository(ZucchininSearchClient zucchininSearchClient)
         {
-            _searchClient = searchClient;
+            _zucchininSearchClient = zucchininSearchClient;
         }
 
-        public async Task<string> SearchGetResultAsync(string searchTerm)
+        public async Task<IEnumerable<ArticlesSearchResultDTO>> SearchGetResultAsync(string query)
         {
-            return await _searchClient.SearchAsync(searchTerm);
+            var response = await _zucchininSearchClient.Client.SearchAsync<ArticlesSearchResultDTO>(query);
+
+            var results = new List<ArticlesSearchResultDTO>();
+            await foreach (var result in response.Value.GetResultsAsync())
+            {
+                results.Add(result.Document);
+            }
+            return results;
         }
     }
 }
