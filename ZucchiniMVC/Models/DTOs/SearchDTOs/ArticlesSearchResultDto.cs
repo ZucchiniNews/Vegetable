12365f7ecabad40aa2eb2da1444ebb9d57c147a7
odinous95 <mjd.thif@icloud.com>
2026-05-07 16:34:06 +0100

    Add ArticlesSearchResultDTO class for article search results
    Introduced the ArticlesSearchResultDTO class in the new Zucchinimvc.Models.DTOs.SearchDTOs namespace. This DTO includes Id, Title, and Slug properties to represent article search results. Title and Slug are initialized to empty strings.

---
+++ ZucchiniMVC/Models/DTOs/SearchDTOs/ArticlesSearchResultDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Zucchinimvc.Models.DTOs.SearchDTOs
+{
+    public class ArticlesSearchResultDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+    }
+}
