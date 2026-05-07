80322f7a3ab2c8959cd15ec12de04c9f92326fd8
Myasar Kamala <mesokamala@ymail.com>
2026-05-07 15:10:33 +0200

    Searchviewcomponent and layout added

---
+++ ZucchiniMVC/ViewComponents/SearchViewComponent .cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zucchinimvc.Infrastructure.Services;
+
+namespace Zucchinimvc.ViewComponents
+{
+    public class SearchViewComponent : ViewComponent
+    {
+        private readonly ISearchService _searchService;
+
+        public SearchViewComponent(ISearchService searchService)
+        {
+            _searchService = searchService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string query)
+        {
+            var results = await _searchService.SearchArticlesByTitleAsync(query);
+            return View(results);
+        }
+    }
+}
