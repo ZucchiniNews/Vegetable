674467df110b1011b85d6f51ef52f137563a0766
odinous95 <mjd.thif@icloud.com>
2026-05-11 15:13:48 +0100

    Add NewsletterProcessor function and update dependencies
    Added NewsletterProcessor Azure Function to process newsletter emails via Service Bus and Resend API. Updated project dependencies, added required packages, removed unused ones, and cleaned up project file content.

---
+++ ZucchiniBackgroundJobs/Functions/NewsletterProcessor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Resend;
+using System.Text.Json;
+using ZucchiniCore.Entities;
+
+namespace ZucchiniBackgroundJobs.Functions
+{
+    public class NewsletterProcessor
+    {
+        private readonly IResend _resend;
+
+        public NewsletterProcessor(IResend resend)
+        {
+            _resend = resend;
+        }
+
+        [Function("NewsletterProcessor")]
+        public async Task Run(
+            [ServiceBusTrigger(
+            "newsletter-emails",
+            Connection = "ServiceBusConnection")]
+        string message)
+        {
+            var job = JsonSerializer.Deserialize<NewsletterEmailJob>(message);
+
+            var email = new EmailMessage
+            {
+                From = "newsletter@yourdomain.com",
+                To = job!.Email,
+                Subject = job.Subject,
+                HtmlBody = job.HtmlBody
+            };
+
+            await _resend.EmailSendAsync(email);
+        }
+    }
+}
