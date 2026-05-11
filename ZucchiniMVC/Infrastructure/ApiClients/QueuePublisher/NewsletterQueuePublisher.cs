e867b748ea0787bc0908521b264fd06adfa2e6ca
odinous95 <mjd.thif@icloud.com>
2026-05-11 15:14:36 +0100

    Add NewsletterQueuePublisher for Azure Service Bus integration
    Introduced NewsletterQueuePublisher to publish NewsletterEmailJob messages to the "newsletter-emails" Azure Service Bus queue. Jobs are serialized to JSON and sent with the DeliveryId as the message ID.

---
+++ ZucchiniMVC/Infrastructure/ApiClients/QueuePublisher/NewsletterQueuePublisher.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Messaging.ServiceBus;
+using System.Text.Json;
+using ZucchiniCore.Entities;
+
+namespace Zucchinimvc.Infrastructure.ApiClients.QueuePublisher
+{
+    public class NewsletterQueuePublisher
+    {
+        private readonly ServiceBusSender _sender;
+
+        public NewsletterQueuePublisher(ServiceBusClient client)
+        {
+            _sender = client.CreateSender("newsletter-emails");
+        }
+
+        public async Task PublishAsync(NewsletterEmailJob job)
+        {
+            var json = JsonSerializer.Serialize(job);
+
+            var message = new ServiceBusMessage(json)
+            {
+                MessageId = job.DeliveryId.ToString()
+            };
+
+            await _sender.SendMessageAsync(message);
+        }
+    }
+}
