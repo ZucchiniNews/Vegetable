6996dfc3772d29e4da10863a45f7157b907442e8
Myasar Kamala <mesokamala@ymail.com>
2026-05-11 10:13:00 +0200

    simple cookies just vc, default and layout

---
+++ ZucchiniMVC/ViewComponents/CookieConsentViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zucchinimvc.ViewComponents
+{
+    public class CookieConsentViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var conseted = Request.Cookies.ContainsKey("CookieConsent");
+            if (conseted) {
+                return Content(string.Empty);
+            }
+            return View("Default", new object());
+        }
+    }
+}
