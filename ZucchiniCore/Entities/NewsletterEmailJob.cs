0b8adf8f1d7b2299f19f85b8be417d4b566242da
odinous95 <mjd.thif@icloud.com>
2026-05-11 15:13:24 +0100

    Add NewsletterEmailJob entity class
    Introduced NewsletterEmailJob in ZucchiniCore.Entities to encapsulate campaign, delivery, recipient, subject, and HTML body details for newsletter email jobs.

---
+++ ZucchiniCore/Entities/NewsletterEmailJob.cs
@@ -0,0 +1,15 @@
+﻿namespace ZucchiniCore.Entities
+{
+    public class NewsletterEmailJob
+    {
+        public Guid CampaignId { get; set; }
+
+        public Guid DeliveryId { get; set; }
+
+        public string Email { get; set; } = default!;
+
+        public string Subject { get; set; } = default!;
+
+        public string HtmlBody { get; set; } = default!;
+    }
+}
