484c539015f79da0c986e950e184da2fe7e29c64
odinous95 <mjd.thif@icloud.com>
2026-05-13 12:01:37 +0100

    Rename Function1 to Newsletter and update logger types
    Refactored the class name from Function1 to Newsletter, updated logger types and constructor accordingly, changed the Function attribute, and added Azure.Storage.Queues.Models using directive for QueueMessage support.

--- zucchini-functions/Newsletter.cs
+++ zucchini-functions/Newsletter.cs
@@ -5,16 +5,16 @@ using Microsoft.Extensions.Logging;
 
 namespace zucchini_functions;
 
-public class Function1
+public class Newsletter
 {
-    private readonly ILogger<Function1> _logger;
+    private readonly ILogger<Newsletter> _logger;
 
-    public Function1(ILogger<Function1> logger)
+    public Newsletter(ILogger<Newsletter> logger)
     {
         _logger = logger;
     }
 
-    [Function(nameof(Function1))]
+    [Function(nameof(Newsletter))]
     public void Run([QueueTrigger("newsletterqueue", Connection = "DefaultEndpointsProtocol=https;AccountName=zucchinibag;AccountKey=xwYp4c5ZhlGo+G+m29aHrgjmIYLhhDrKIrMKPZJaGcRTPM6ZZXoiFBdeco:vzhfANY4YzMtAOfMj+AStpXm1IA==;EndpointSuffix=core.windows.net")] QueueMessage message)
     {
         _logger.LogInformation("C# Queue trigger function processed: {messageText}", message.MessageText);
