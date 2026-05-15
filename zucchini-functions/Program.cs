a82347333e932acc7118d82028cfd167af84711a
odinous95 <mjd.thif@icloud.com>
2026-05-15 15:19:47 +0100

    optamiztions for deployment

--- zucchini-functions/Program.cs
+++ zucchini-functions/Program.cs
@@ -1,20 +1,11 @@
 using Azure.Storage.Queues;
 using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Resend;
-using Zucchinimvc.Application.Services.Logger;
 using Zucchinimvc.Application.Services.QueuePublishier.NewLetterQueue;
 using Zucchinimvc.Application.Services.UsersService;
-using Zucchinimvc.Application.Services.Weather;
-using Zucchinimvc.Infrastructure.ApiClients.AzureTableClient;
-using Zucchinimvc.Infrastructure.ApiClients.WeatherClient;
-using Zucchinimvc.Infrastructure.Config;
-using Zucchinimvc.Infrastructure.Data;
-using Zucchinimvc.Infrastructure.Repositories.HistoryRepo;
-using Zucchinimvc.Infrastructure.Repositories.WeatherRepo;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 builder.ConfigureFunctionsWebApplication();
@@ -40,17 +31,5 @@ builder.Services.AddTransient<QueueClient>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IWeeklyNewsLetterPublisher, WeeklyNewsLetterPublisher>();
 
-
-
-// Weather Related services and repositories would be registered here, similar to the previous example
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.Configure<WeatherSettings>(builder.Configuration.GetSection("WeatherApi"));
-builder.Services.AddHttpClient<WeatherClient>();
-builder.Services.AddSingleton<IAzureTableClient, AzureTableClient>();
-builder.Services.AddScoped<IWeatherService, WeatherService>();
-builder.Services.AddScoped<IApiLoggerService, ApiLoggerService>();
-builder.Services.AddScoped(typeof(IHistoryRepository<>), typeof(HistoryRepository<>));
-builder.Services.AddScoped<IWeatherRepository, WeatherRepository>();
 
 builder.Build().Run();
--- zucchini-functions/config/NewsLetterSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace zucchini_functions.Config
-{
-    public class NewsLetterSettings
-    {
-        public string ApiToken { get; set; } = string.Empty;
-        public string FromEmail { get; set; } = string.Empty;
-    }
-}
