1c74c3074657051691cd03deb8dcfc80636eebd0
odinous95 <mjd.thif@icloud.com>
2026-05-15 17:43:30 +0100

    newsletterWeeklypublisher  DI regiseration

--- zucchini-functions/Program.cs
+++ zucchini-functions/Program.cs
@@ -1,27 +1,27 @@
-using Azure.Storage.Queues;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Resend;
 using Zucchinimvc.Application.Services.QueuePublishie.WelcomeToNewsLetterEmail;
 using Zucchinimvc.Application.Services.QueuePublishier.WeeklyNewsLetterEmail;
 using Zucchinimvc.Application.Services.UsersService;
+using Zucchinimvc.Infrastructure.ApiClients.QueuePublisher;
+using Zucchinimvc.Infrastructure.Config;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
-
-builder.Services.AddTransient(sp =>
+builder.Services.AddTransient<IWeeklyNewsLetterPublisher>(sp =>
 {
-    var connectionString =
-        builder.Configuration["AzureWebJobsStorage"];
-
-    var queueName =
-        builder.Configuration["QueueName"]!;
-
-    return new QueueClient(connectionString, queueName);
+    var options = sp.GetRequiredService<IOptions<WeeklyNewsLetterQueueSettings>>();
+    var settings = options.Value;
+    var queueClient = new ZucchiniQueueClient(settings.ConnectionString, settings.QueueName);
+    return new WeeklyNewsLetterPublisher(queueClient);
 });
+
+
 
 builder.Services.AddHttpClient();
 
