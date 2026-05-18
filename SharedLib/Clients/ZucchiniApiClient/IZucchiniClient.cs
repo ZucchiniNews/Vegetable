b295d00c681dfdae708e39827eeb77db4c8ec471
odinous95 <mjd.thif@icloud.com>
2026-05-18 12:34:08 +0100

    Add ZucchiniClient for fetching subscribed users
    Introduced `IZucchiniClient` interface and its implementation
    in `ZucchiniClient` to fetch subscribed users via HTTP.
    Integrated `IConfiguration` for API key management.
    Updated `SharedLib.csproj` to include the required
    `Microsoft.Extensions.Configuration` package.

---
+++ SharedLib/Clients/ZucchiniApiClient/IZucchiniClient.cs
@@ -0,0 +1,10 @@
+﻿using SharedLib.QueuePublishier.DTOs;
+
+namespace zucchini_functions.Clients.ZucchiniApiClient
+{
+    public interface IZucchiniClient
+    {
+        Task<List<NewsletterSubscriberDto>> GetSubscribedUsersAsync();
+    }
+
+}
