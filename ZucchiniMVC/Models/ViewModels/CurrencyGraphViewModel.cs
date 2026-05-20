28e0a11253cf452c6e48a51e354f014d86a87dd6
Myasar Kamala <mesokamala@ymail.com>
2026-05-20 10:23:14 +0200

    ViewModel

---
+++ ZucchiniMVC/Models/ViewModels/CurrencyGraphViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Zucchinimvc.Models.ViewModels
+{
+    public class CurrencyGraphViewModel
+    {
+        public string BaseCurrency { get; set; } = string.Empty;
+        public Dictionary<string, decimal> Rates { get; set; } = new();
+    }
+}
