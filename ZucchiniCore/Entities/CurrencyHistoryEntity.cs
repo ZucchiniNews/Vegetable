479721e81e46dc4fe93f8a0bf692f87dfd7eba90
Myasar Kamala <mesokamala@ymail.com>
2026-05-20 13:16:27 +0200

    Fixing data

--- ZucchiniCore/Entities/CurrencyHistoryEntity.cs
+++ ZucchiniCore/Entities/CurrencyHistoryEntity.cs
@@ -2,5 +2,14 @@
 {
     public class CurrencyHistoryEntity
     {
+        public int Id { get; set; }
+
+        public string BaseCurrency { get; set; } = "USD";
+
+        public string TargetCurrency { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; }
+
+        public decimal Rate { get; set; }
     }
 }
